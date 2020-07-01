f253aaad8d17caa8ec3b0c7533dc840afe8f4cb7
Sherin Binu <sherinbinu@hotmail.com>
2020-07-02 01:34:12 +0530

    crawl through pages

--- classes/Crawler.js
+++ classes/Crawler.js
@@ -1,20 +1,49 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 module.exports = class Crawler {
   constructor() {
     this.stop = false
   }
 
-  start(client) {
+  start(client, limit=0) {
+    console.log('on page ' + ((limit / 50) + 1))
     this.logChannel = client.channels.cache.get('593610953363554314')
-    console.log('running', this.stop)
-    this.logChannel.send('running')
+    axios.get(`https://myanimelist.net/character.php?limit=${limit}`).then((response) => {
+      const $ = cheerio.load(response.data)
+      const urls = []
+      $('.ranking-list td.people .information a').each((index, el) => {
+        urls.push(el.attribs['href'])
+      })
+      console.log(urls)
+      Promise.all(urls.map(url => axios.get(url)))
+        .then(response => {
+          const content = response.map(res => {
+            const $ = cheerio.load(res.data)
+            return $('#content').text()
+          })
+          this.logChannel.send('running')
+  
+          if(!this.stop) {
+            setTimeout(() => {
+              process.nextTick(this.start.bind(this, client, limit + 50))
+            }, 0)
+          } else {
+            console.log('stopped')
+          }
+        })
+        .catch(() => {
+  
+          if(!this.stop) {
+            setTimeout(() => {
+              process.nextTick(this.start.bind(this, client, limit))
+            }, 0)
+          } else {
+            console.log('stopped')
+          }
 
-    if(!this.stop) {
-      setTimeout(() => {
-        process.nextTick(this.start.bind(this, client))
-      }, 3000)
-    } else {
-      console.log('stopped')
-    }
+        })
+    })
   }
 
   stopExecution() {
